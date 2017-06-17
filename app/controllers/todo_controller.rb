9440c82b4b2585dbafac3ef5e6c668abd3bd4f35
Ho Kan Leung <hokan.leung@amethodschools.org>
2017-06-17 20:50:38 +0000

    Define instance variables in todo show action, and place in show view

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,10 @@
 class TodoController < ApplicationController
+    def show
+         @todo_description = "Make the curriculum"
+         @todo_pomodoro_estimate = 2
+    end
     def index
     end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 get 'todo/index', to: "todo#index"
+get 'todo/show', to: "todo#show"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
