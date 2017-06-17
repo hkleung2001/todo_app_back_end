aa40ceb7333714cf5427f4ff8121308df9e73d8e
Ho Kan Leung <hokan.leung@amethodschools.org>
2017-06-17 21:18:09 +0000

    Add dynamic route segments

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,7 +1,8 @@
 class TodoController < ApplicationController
     def show
-         @todo_description = "Make the curriculum"
+         @todo_description = "New Curriculum"
          @todo_pomodoro_estimate = 2
+         @todo_estimate_time = 15
     end
     def index
     end
