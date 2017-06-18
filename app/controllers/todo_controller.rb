97296c8af0330378bdc355e881b74023fdefed62
Ho Kan Leung <hokan.leung@amethodschools.org>
2017-06-18 17:19:16 +0000

    Install postgres and update database.yml

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,11 +1,19 @@
 class TodoController < ApplicationController
-    def show
-         @todo_description = "New Curriculum"
-         @todo_pomodoro_estimate = 2
-         @todo_estimate_time = 15
-    end
     def index
     end
+    
+    def show
+        todo_id = params[:id] 
+        if todo_id == '1'
+         @todo_description = "Goal #1"
+         @todo_pomodoro_estimate = 3
+         @todo_estimate_time = 12
+         
+        elsif todo_id == '2'
+         @todo_description = "Goal #2"
+         @todo_pomodoro_estimate = 4
+         @todo_estimate_time = 10
+         
+        end
+    end
 end
-
-
